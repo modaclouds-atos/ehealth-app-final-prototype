e6453f07fe612450f653112c28afb8d6f36514ac
a572832 <roi.suicasas.external@atos.net>
2015-11-03 11:52:10 +0100

    index page updated

---
+++ ehealth-webgui/ehealth-modaclouds-gui/WebContent/index2/js/index.js
@@ -0,0 +1,24 @@
+// collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+//page scrolling feature - requires the jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+//  closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
